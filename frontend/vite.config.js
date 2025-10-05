bed5c3286edb865c26f6f5d88b95552f015aefe7
M Shamitha <shamitha0723@gmail.com>
2025-10-05 18:29:04 +0530

    Configure Vite to build assets to static directory

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'static',
+  },
   define: {
     'import.meta.env.VITE_GOOGLE_CLIENT_ID': JSON.stringify('770268825896-a4evs6pveo1cah6ks3fddllmlesqgkot.apps.googleusercontent.com'),
   },
